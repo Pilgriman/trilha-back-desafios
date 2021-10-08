d8342495d2f714a49d03a8b90ab6c997b25c5cfc
Andre B <limaufrpe289@gmail.com>
2021-10-08 01:11:26 -0300

    Delete financys directory

--- financys/src/main/java/trilha/back/financys/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package trilha.back.financys.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import trilha.back.financys.category.Category;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
--- financys/src/main/java/trilha/back/financys/repository/EntryRepository.java
+++
@@ -1,7 +0,0 @@
-package trilha.back.financys.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import trilha.back.financys.entry.Entry;
-
-public interface EntryRepository extends JpaRepository<Entry, Long> {
-}
