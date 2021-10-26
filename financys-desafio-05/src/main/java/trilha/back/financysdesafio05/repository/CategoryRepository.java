07728c85f7153bd587c172be1c95fa912b44121f
Andre B <limaufrpe289@gmail.com>
2021-10-26 11:03:25 -0300

    Delete financys-desafio-05 directory

--- financys-desafio-05/src/main/java/trilha/back/financysdesafio05/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package trilha.back.financysdesafio05.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import trilha.back.financysdesafio05.entities.Category;
-
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-
-}
--- financys-desafio-05/src/main/java/trilha/back/financysdesafio05/repository/EntryRepository.java
+++
@@ -1,7 +0,0 @@
-package trilha.back.financysdesafio05.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import trilha.back.financysdesafio05.entities.Entry;
-
-public interface EntryRepository extends JpaRepository<Entry, Long> {
-}
