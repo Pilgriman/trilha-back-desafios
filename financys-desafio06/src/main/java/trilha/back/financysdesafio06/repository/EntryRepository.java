d8be8a83432a43d585265824e8b1a2c2fe3b795f
Andre B <limaufrpe289@gmail.com>
2021-11-02 16:30:19 -0300

    Delete financys-desafio06 directory

--- financys-desafio06/src/main/java/trilha/back/financysdesafio06/repository/EntryRepository.java
+++
@@ -1,9 +0,0 @@
-package trilha.back.financysdesafio06.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import trilha.back.financysdesafio06.entities.Entry;
-
-public interface EntryRepository extends JpaRepository<Entry, Long> {
-
-
-}
